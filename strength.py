4c975c421e65bde92b83a4687afe26b03ce7f932
Vishwa <92665375+vishwa-infosec@users.noreply.github.com>
2022-09-05 21:21:57 +0530

    Update strength.py

--- strength.py
+++ strength.py
@@ -1,47 +1,32 @@
-def strength(password):
-    # initialize boolean variables
-    lowercase = False
-    uppercase = False
-    numbers = False
-    special = False
-    # initialize percentage as 0 to change later
-    percentage = 0
-    # iterate letters in password to check if it contains lower,upper,number,special character
-    for letter in password:
-        if letter in "abcdefghijklmnopqrstuvwxyz":
-            lowercase = True
-        if letter in "ABCDEFGHIJKLMNOPQRSTUVWXYZ":
-            uppercase = True
-        if letter in "1234567890":
-            numbers = True
-        if letter in "!@#$%^&*()_-+=|\}{][':;>.<,?/`~:/":
-            special = True
-    # add percentage of password according to conditions
-    if lowercase == True:
-        percentage += 20
-    if uppercase == True:
-        percentage += 20
-    if numbers == True:
-        percentage += 20
-    if special == True:
-        percentage += 20
-    if len(password) >= 16:
-        percentage += 20
-    print("Your password is {} percent Strong\n".format(percentage))
-    if lowercase == True and uppercase == True and numbers == True and special == True:
-        print("Your Passwrd is highly strong\n")
-    if lowercase == False:
-        print("Lowercase is missing in your password\n")
-    if uppercase == False:
-        print("Uppercase is missing in your password\n")
-    if numbers == False:
-        print("Number is missing in your password\n")
-    if special == False:
-        print("Special Character is missing in your password\n")
-    if len(password) < 16:
-        print("Hackers nowadays are capable of compromising password that are less than 16 digit, We recommend using atleast 16 digit password\n")
+import string
+password = input("Enter password:")
+lower = string.ascii_lowercase
+upper = string.ascii_uppercase
+digit = string.digits
+symbol = string.punctuation
+isalpha = 0
+isalphasmall = 0
+isdigit = 0
+issymbol = 0
+for i in password:
+    if i in lower:
+        isalpha = 1
+    elif i in upper:
+        isalphasmall = 1
+    elif i in digit:
+        isdigit = 1
+    elif i in symbol:
+        issymbol = 1
 
+total = isalpha + isalphasmall + isdigit + issymbol
 
-# get input of password from user
-password = input("Enter Your Password: ")
-strength(password)
+if total == 4:
+    print("Password is very strong")
+elif total == 3:
+    print("Password is strong")
+elif total == 2:
+    print("Password is average")
+elif total == 1:
+    print("password is weak")
+elif total == 0:
+    print("Password is very weak")
